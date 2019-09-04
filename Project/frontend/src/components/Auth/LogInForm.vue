6817d68935207790a2ed61dcf2f2967c5b30cef2
mouni krishna <mounikrishnaa@gmail.com>
2019-09-03 22:49:09 -0500

    Add template for LogInForm.vue

--- Project/frontend/src/components/Auth/LogInForm.vue
+++ Project/frontend/src/components/Auth/LogInForm.vue
@@ -1,3 +1,43 @@
 <template>
+  <div class="ui stackable three column centered grid container">
+    <div class="column">
+      <h2 class="ui dividing header">Log In</h2>
 
+      <Notification
+        :message="notification.message"
+        :type="notification.type"
+        v-if="notification.message"
+      />
+
+      <form class="ui form" @submit.prevent="login">
+        <div class="field">
+          <label>Email</label>
+          <input type="email" name="email" v-model="email" placeholder="Email" required>
+        </div>
+
+        <div class="field">
+          <label>Password</label>
+          <input type="password" name="password" v-model="password" placeholder="Password" required>
+        </div>
+
+        <button class="fluid ui primary button">LOG IN</button>
+
+        <div class="ui hidden divider"></div>
+      </form>
+
+      <div class="ui divider"></div>
+
+      <div class="ui column grid">
+        <div class="center aligned column">
+          <p>
+            Don't have an account? <router-link to="/signup">Sign Up</router-link>
+          </p>
+          <p>
+            Forgot password? <router-link to="/resetpassword">Reset Here</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
