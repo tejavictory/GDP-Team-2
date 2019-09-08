4ec952e97ab6386d1e5fb6066026980e92e8421d
Karthik <Karthikmuthyala20@gmail.com>
2019-09-07 21:49:32 -0500

    Add template for InsDash.vue

--- Project/frontend/src/components/InsDash.vue
+++ Project/frontend/src/components/InsDash.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div class="ui container">
+        <p> Instructor Dashboard Construction in progress... </p>
+        <div class="ui top attached button" tabindex="0" v-on:click="showModal">Add Course</div>
+        <br/>
+        <div class="ui stackable four column grid">
+            <InsCourse/>
+            <InsCourse/>
+        </div>
+        <CourseModal/>
+    </div>
 </template>
+
+
+
