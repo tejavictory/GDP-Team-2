2304b93a384364d073dc0254328ad9867e7be51b
mouni krishna <mounikrishnaa@gmail.com>
2019-09-07 22:26:54 -0500

    Add template for InsNav.vue

--- Project/frontend/src/components/InsNav.vue
+++ Project/frontend/src/components/InsNav.vue
@@ -1,3 +1,23 @@
 <template>
-    
+
+<div class="ui stackable container big menu">
+
+  <div class="header item">
+
+    Codeword App
+
+  </div>
+
+  <div class="right menu">
+
+    <a class="item">
+
+      Logout
+
+    </a>
+
+  </div>
+
+</div>
+
 </template>
