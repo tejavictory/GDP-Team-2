a74fde0df616f46ae82958120d72183c6dc81335
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-09-08 23:43:12 -0500

    Add form in template

--- Project/frontend/src/components/CourseModal.vue
+++ Project/frontend/src/components/CourseModal.vue
@@ -8,4 +8,103 @@
             Add a New Course
 
         </div>
+        <div class="ui form">
+
+            <div class="fields">
+
+                <div class="field">
+
+                <label>Course Name</label>
+
+                <input type="text" placeholder="Course Name">
+
+                </div>
+
+                <div class="field">
+
+                <label>Start Date</label>
+
+                    <div class="ui calendar" id="startdate_calendar">
+
+                        <div class="ui input left icon">
+
+                            <i class="calendar icon"></i>
+
+                            <input type="text" placeholder="Start Date" v-on:click="startdatepick">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="field">
+
+                <label>End Date</label>
+
+                    <div class="ui calendar" id="enddate_calendar">
+
+                        <div class="ui input left icon">
+
+                            <i class="calendar icon"></i>
+
+                            <input type="text" placeholder="Start Date" v-on:click="enddatepick">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="fields">
+
+                <div class="field">
+
+                <label>Pre-Survey Link</label>
+
+                <input type="text" placeholder="Pre-Survey Link">
+
+                </div>
+
+                <div class="field">
+
+                <label>Post-Survey Link</label>
+
+                <input type="text" placeholder="Post-Survey Link">
+
+                </div>      
+
+            </div>
+
+            <div class="fields">
+
+                <div class="field">
+
+                    <label> Add Students to the Course </label>
+
+                    <div class="ui left icon input">
+
+                        <input type="text" placeholder="Add Students...">
+
+                        <i class="users icon"></i>
+
+                    </div>                    
+
+                </div>      
+
+ 
+
+                <div class="field">
+
+                    <label> Upload a CSV file instead </label>
+
+                    <input type="file" placeholder="File Upload">
+
+                </div>
+
+            </div>
+
+        </div>
 </template>
