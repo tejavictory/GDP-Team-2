f015749cf61db9c5e483a269a5a3e8e30ad71133
Kancharla, Sai Krishna Teja <42943403+tejavictory@users.noreply.github.com>
2019-09-10 23:47:13 -0500

    Add template for Admin Nav

---
+++ Project/frontend/src/components/AdminNav.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<div class="ui pointing stackable container big menu">
+  <div class="item">
+    <!-- <img src="/images/logo.png"> -->
+    <span class="navhead"> Admin </span>
+  </div>
+  <a class="item" v-on:click="setActive('codeword')" :class="{ active: isActive('codeword') }">Codeword Sets</a>
+  <a class="item" v-on:click="setActive('rmcourse')" :class="{ active: isActive('rmcourse') }">Remove Courses</a>
+  <a class="item" v-on:click="setActive('insreq')" :class="{ active: isActive('insreq') }">Instructor Requests</a>
+  <a class="item" v-on:click="setActive('rmusers')" :class="{ active: isActive('rmusers') }">Remove Users</a>
+  <div class="right menu">
+    <a class="item">
+      Logout
+    </a>
+  </div>
+</div>
+</div>
+</template>
