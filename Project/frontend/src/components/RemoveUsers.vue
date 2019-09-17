2f1f8fb6a4137f89fbe04dbbe72de31d9d208ffa
mouni krishna <mounikrishnaa@gmail.com>
2019-09-16 23:24:47 -0500

    Add template to remove users vue file

--- Project/frontend/src/components/RemoveUsers.vue
+++ Project/frontend/src/components/RemoveUsers.vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <div>
+
+        <br/>
+
+        <div class="ui container">
+
+            <h2>Remove Users.... Under Construction</h2>
+
+        </div>
+
+    </div>
 </template>
