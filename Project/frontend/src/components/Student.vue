466d14a596d0b583968a0f863820bc30018c6fb4
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-09-17 15:31:33 -0500

    Create template in Student.vue

--- Project/frontend/src/components/Student.vue
+++ Project/frontend/src/components/Student.vue
@@ -1,3 +1,41 @@
 <template>
-    
+    <div class="ui container">
+
+        <div class="ui pointing stackable container big menu">
+
+        <div class="header item">
+
+            Codeword App
+
+        </div>
+
+            <a class="item active" >
+
+            Student Dashboard
+
+            </a>
+
+        <div class="right menu">
+
+            <a class="item" @click="logout">
+
+            Logout
+
+            </a>
+
+        </div>
+
+        </div>
+
+        <p> Student Dashboard Construction in progress... </p>
+
+        <div class="ui stackable four column grid">
+
+            <StuCourse/>
+
+            <StuCourse/>
+
+        </div>
+
+    </div>
 </template>
