18be5e923aa023a2cc4a8b9abad157f827b75afb
Karthik <Karthikmuthyala20@gmail.com>
2019-09-20 02:01:30 -0500

    Add fetchcources

--- Project/frontend/src/components/InsDash.vue
+++ Project/frontend/src/components/InsDash.vue
@@ -1,39 +1,99 @@
 <template>
+
     <div class="ui container">
+
         <p> Instructor Dashboard Construction in progress... </p>
+
         <div class="ui top attached button" tabindex="0" v-on:click="showModal">Add Course</div>
+
         <br/>
-        <div class="ui stackable four column grid">
-            <InsCourse/>
-            <InsCourse/>
+
+        <div class="ui stackable four column grid">            
+
+            <InsCourse
+
+                v-for="item in courses"
+
+                :key="item.id"
+
+                :course="item"
+
+            />
+
         </div>
+
         <CourseModal/>
+
     </div>
+
 </template>
 
+ 
+
 <script>
+
 import InsCourse from './InsCourse.vue'
+
 import CourseModal from './CourseModal.vue'
 
+ 
+
 export default {
+
     name: 'InsDash',
-    components: {
-        InsCourse,
-        CourseModal
+
+    data() {
+
+        return{
+
+            courses: []
+
+        }
+
     },
-     mounted:function() {
+
+    components: {
+
+        InsCourse,
+
+        CourseModal
+
+    },
+
+    mounted:function() {
 
         this.fetchCourses()
 
     },
+
     methods: {
+
         showModal: function() {
+
             $('.ui.modal').modal('show')
+
+        },
+
+        fetchCourses() {
+
+            axios.get('/courses').then(response => {
+
+                    this.courses = response.data.data
+
+            })
+
         }
+
     }
+
 }
+
 </script>
 
+ 
+
 <style scoped>
+
+ 
 
 </style>
