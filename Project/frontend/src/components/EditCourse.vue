91cfd25b65d74c7c11a8fa495ed6101630ea2872
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-09-22 22:23:08 -0500

    Add header to EditCourse.vue

--- Project/frontend/src/components/EditCourse.vue
+++ Project/frontend/src/components/EditCourse.vue
@@ -1,3 +1,12 @@
 <template>
+<div class="ui modal" id="editCourse">
+
+        <i class="close icon"></i>
+
+        <div class="header">
+
+            Edit Course
+
+        </div>
     
 </template>
