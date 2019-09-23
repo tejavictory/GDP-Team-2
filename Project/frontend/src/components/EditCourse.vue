ad9db5431fdca5bf995659fb9015c1bb7a99522b
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-09-22 22:39:31 -0500

    Add label course_name

--- Project/frontend/src/components/EditCourse.vue
+++ Project/frontend/src/components/EditCourse.vue
@@ -8,5 +8,16 @@
             Edit Course
 
         </div>
+        <div class="ui form" @submit.prevent="saveCourse">
+
+            <div class="fields">
+
+                <div class="field">
+
+                <label>{{ course.course_name }}</label>
+
+                <input type="text" v-model="cname" placeholder="Course Name">
+
+                </div>
     
 </template>
