5432630c6d46b4f483e9266c9581b617dd32b61e
Karthik <Karthikmuthyala20@gmail.com>
2019-09-25 01:42:52 -0500

    Add changes to stuCourse.vue

--- Project/frontend/src/components/StuCourse.vue
+++ Project/frontend/src/components/StuCourse.vue
@@ -1,24 +1,83 @@
 <template>
+
     <div class="column">
+
             <div class="ui fluid card">
+
                 <div class="content">
-                    <div class="header">Course Title</div>
+
+                    <div class="header">{{ course.course_name }}</div>
+
                 </div>
+
                 <div class="content">
-                    <p> Start Date: </p>
-                    <p> End Date: </p>
-                    <p> Pre-Survey Link: </p>
-                    <p> Post-Survey Link: </p>
-                    <p> Codeword: </p>
+
+                    <label> Start Date: </label> <span>{{ course.startDate }}</span><br/>
+
+                    <label> End Date: </label> <span>{{ course.endDate }}</span><br/>
+
+                    <label> Pre-Survey Link: </label> <span>{{ course.presurveylink }}</span><br/>
+
+                    <label> Post-Survey Link: </label> <span>{{ course.postsurveylink }}</span><br/>
+
+                    <label> Codeword: </label> <span>{{  }}</span><br/>
+
                 </div>
+
             </div>
+
     </div> 
 
+ 
+
     
+
 </template>
 
+ 
+
 <script>
+
 export default {
-    name: 'StuCourse'
+
+    name: 'StuCourse',
+
+    props: {
+
+        course: {
+
+            type: Object,
+
+            required: true
+
+        }
+
+    }
+
 }
+
 </script>
+
+ 
+
+<style scoped>
+
+*{
+
+    font-family: 'Quicksand', sans-serif;
+
+}
+
+label{
+
+    font-weight: bold;
+
+}
+
+.ui.icon.button{
+
+    float: right;
+
+}
+
+</style>
