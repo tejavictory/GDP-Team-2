5aabfff7cc9fbd12e2b089bb194a2ff9967d83fa
mouni krishna <mounikrishnaa@gmail.com>
2019-09-24 20:43:29 -0500

    clear errors in instructor course

--- Project/frontend/src/components/InsCourse.vue
+++ Project/frontend/src/components/InsCourse.vue
@@ -1,26 +1,42 @@
 <template>
+
+    <div class="column">
+
+            <div class="ui fluid card">
+
+                <div class="content">
+
+                    <label>{{ course.course_name }}</label> 
+
+                    <button class="ui icon button">
+
+                        <i class="trash alternate outline icon"></i>
+
+                    </button>
+
+                    <button class="ui icon button">
+
+                        <i class="edit outline icon"></i>
+
+                    </button>
+
+                     
+
+                </div>
+
+               
+            </div>
+
+    </div> 
+
+ 
+
     
+
 </template>
 
+ 
 
-<script>
 
-export default {
+ 
 
-    name: 'InsCourse',
-
-    props: {
-
-        course: {
-
-            type: Object,
-
-            required: true
-
-        }
-
-    }    
-
-}
-
-</script>
