6b4841abc97bb46b72dff29f0e7f476b47100bcc
preethampotu <preetham.reddy16@gmail.com>
2019-09-25 01:22:18 -0500

    Add data to thead of RemoveUsers.vue

--- Project/frontend/src/components/RemoveUsers.vue
+++ Project/frontend/src/components/RemoveUsers.vue
@@ -16,62 +16,12 @@
 
                 <th>Name</th>
 
-                <th>Status</th>
+                <th>Role</th>
 
-                <th>Notes</th>
+                <th>Email</th>
+
+                <th></th>
 
                 </tr>
 
             </thead>
-
-            <tbody>
-
-                <tr>
-
-                <td>Jamie</td>
-
-                <td>Approved</td>
-
-                <td>Requires call</td>
-
-                </tr>
-
-                <tr class="active">
-
-                <td>John</td>
-
-                <td>Selected</td>
-
-                <td>None</td>
-
-                </tr>
-
-                <tr>
-
-                <td>Jamie</td>
-
-                <td>Approved</td>
-
-                <td>Requires call</td>
-
-                </tr>
-
-                <tr>
-
-                <td>Jill</td>
-
-                <td>Approved</td>
-
-                <td>None</td>
-
-                </tr>
-
-            </tbody>
-
-            </table>
-
-        </div>
-
-    </div>
-
-</template>
