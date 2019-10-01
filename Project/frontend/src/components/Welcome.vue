15332ad539e40bdb784f3b2bc9c98d8cb3075209
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-09-30 22:59:02 -0500

    create Welcome.vue

---
+++ Project/frontend/src/components/Welcome.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="ui container">
+        <h1> You are logged in successfully </h1>
+        <button class="ui button" v-on:click="getDashboard">Continue to Dashboard</button>
+    </div>
+</template>
