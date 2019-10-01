c372bcb59d63025e3cb65534c7c1f557827db5ae
karthikmuthyala <43012025+karthikmuthyala@users.noreply.github.com>
2019-09-30 19:24:34 -0500

    Add new file InsStu.vue and template

---
+++ Project/frontend/src/components/InsStu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ui container">
+        <p> Student Dashboard Construction in progress... </p>
+        <div class="ui stackable four column grid">
+            <StuCourse
+                v-for="item in courses"
+                :key="item.id"
+                :course="item"
+                />
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+</style>
