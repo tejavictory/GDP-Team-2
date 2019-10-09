3290b59e86e48e7c1fd2f87c2737b47526d239d6
karthikmuthyala <43012025+karthikmuthyala@users.noreply.github.com>
2019-10-09 09:16:06 -0500

    Add changes and functions to codewords.vue

--- Project/frontend/src/components/Codewords.vue
+++ Project/frontend/src/components/Codewords.vue
@@ -1,8 +1,22 @@
 <template>
+    <div class="ui container">
 
+    </div>
+</template>
 
-</template> 
 <script>
-
-
+export default {
+    name: 'Codewords',
+    data () {
+        return {
+        }
+    },
+    components: {
+    },
+    methods: {
+        getcodes: function() {
+            
+        }
+    }
+}
 </script>
