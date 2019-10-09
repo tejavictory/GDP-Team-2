3044ff17001309d486a21ba4313cba00d0b67722
mouni krishna <mounikrishnaa@gmail.com>
2019-10-08 20:02:00 -0500

    Add RemoveCourses.vue file

---
+++ Project/frontend/src/components/RemoveCourses.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <br/>
+        <div class="ui segment container">
+            <h2>Remove Courses.... Under Construction</h2>
+        </div>
+    </div>
+</template>
+
