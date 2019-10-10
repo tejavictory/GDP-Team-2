9497b4347552d96255a2028a945b2b266402909d
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-10-09 21:05:17 -0500

    Add script to Instructor.vue

--- Project/frontend/src/components/Instructor.vue
+++ Project/frontend/src/components/Instructor.vue
@@ -4,3 +4,16 @@
         <router-view/>
     </div>
 </template>
+<script>
+import InsNav from './InsNav.vue'
+import InsDash from './InsDash.vue'
+import InsStu from './InsStu.vue'
+export default {
+    name: 'Instructor',
+    components: {
+        InsNav,
+        InsDash,
+        InsStu
+    }
+}
+</script>
