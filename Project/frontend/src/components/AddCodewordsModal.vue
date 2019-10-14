2d48c1e415745bccd5f89d5f4c9de299fc96ce08
mouni krishna <mounikrishnaa@gmail.com>
2019-10-14 09:14:35 -0500

    made changes in AddCodewordsModal.vue

--- Project/frontend/src/components/AddCodewordsModal.vue
+++ Project/frontend/src/components/AddCodewordsModal.vue
@@ -7,7 +7,7 @@
         <div class="ui form" @submit.prevent="createSet">
             <div class="fields">
                 <div class="field">
-               
+                <label>Codeword Set Name</label>
                 <input type="text" data-tooltip="testing" placeholder="Codeword Set Name" v-model="csname" @change="validateName()">
                 </div>
                 <div class="field" id="nameerr">
@@ -25,14 +25,18 @@
                     <ul>
                         <li>Hard rule violations: {{ this.hardcount }}</li>
                         <li>Soft rule violations: {{ this.softcount }}</li>
+                        <li> SOFT RULE VIOLATIONS ARE INDICATED IN RED TEXT. You can choose to override these and create the set.</li>
+                        <li> HARD RULE VIOLATIONS ARE INDICATED IN RED BACKGROUND. You cannot create a set unless you change these. </li>
                     </ul>
                 </div>
             </div>
              <div class="ui stackable grid" id="grid">
             </div>
-            
+            <div class="field">
+                <button class="ui button blue" v-on:click="addNew"><i class="plus icon"></i>New Codeword</button> 
+            </div>
             <div class="field" >
-                <button class="ui button fluid green" v-on:click="validateWords"></i>Validate</button> 
+                <button class="ui button fluid green" v-on:click="validateWords"><i class="check icon"></i>Validate</button> 
             </div>
         </div>
         <div class="actions">
