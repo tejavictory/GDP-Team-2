8840bab890984a5c6afff6d8fa945bd8de0ec1df
preethampotu <preetham.reddy16@gmail.com>
2019-11-10 21:55:47 -0600

    Update code in CourseController.js file

--- Project/backend/app/Controllers/Http/CourseController.js
+++ Project/backend/app/Controllers/Http/CourseController.js
@@ -50,6 +50,74 @@ class CourseController {
           data: course
         })
       }
+      async getCourses({ request, response, params: { insEmail } }) {
+        const courses = await Course.query().where('insEmail','=',insEmail).fetch()    
+        response.status(200).json({
+          message: 'Here are your courses.',
+          data: courses
+        })
+      }
+
+      async getStudents({ request, response, params: { id } }) {
+        const course = await Course.find(id)
+        const students = await course.users().fetch()
+        response.status(200).json({
+          message: 'Here are your students.',
+          data: students
+        })
+      }
+    
+      async update({ request, response, params: { id } }) {
+        const course_name = request.input()
+
+        var course = Course.find(id)
+        
+        course.course_name = data.course_name
+        course.startDate = data.startDate
+        course.endDate = data.endDate
+        course.presurveylink = data.presurveylink
+        course.postsurveylink = data.postsurveylink
+        course.codewordAssignStatus = data.codewordAssignStatus
+        await course.save()
+    
+        if (users && users.length > 0) {
+          // await course.users().detach()
+          await course.users().attach(users)
+          course.users = await course.users().fetch()
+        }
+    
+        response.status(200).json({
+          message: 'Successfully updated this course.',
+          data: course
+        })
+      }
+
+      async assignSet({ request, response, params: { course_name } }) {
+        const setname = request.input('setname')
+
+        var course = await Course.findBy('course_name',course_name)
+        
+        course.codewordset = setname
+        await course.save()
+        
+        response.status(200).json({
+          message: 'Successfully updated set for this course.',
+          data: course
+        })
+      }
+
+      async distributedwords({ request, response, params: { id } }) {
+
+        var course = await Course.find(id)
+        
+        course.distributed = 1
+        await course.save()
+        
+        response.status(200).json({
+          message: 'Successfully distributed for this course.',
+          data: course
+        })
+      }
     
 
 
